0f56c445a1d328e3221061cbae613df0d8745156
Patrick Cao <patrick.cao8@gmail.com>
2014-07-09 18:03:31 -0400

    Successfully built buffer_t on server

--- Research/cross/fib.cpp
+++ Research/cross/fib.cpp
@@ -3,13 +3,15 @@
 #include <math.h>
 
 using namespace Halide;
+#include "image_io.h"
+
 using namespace std;
 
 int main(){
 	Var x;
 	
 	Func fib;
-	fib(x) = 1;
+	fib(x) = cast<uint8_t>(1);
 	RDom r (2,15);
 	fib(r) = fib(r-1) + fib(r-2);
 	
@@ -17,12 +19,13 @@ int main(){
 
 	
 	Func f;
-	f(x) = tan(-1.0f);
-	Image<float> test = f.realize(50);
-	cout << test(45) << endl;
+	f(x) = cast<uint8_t>(8);
+	Image<uint8_t> test = f.realize(50);
+	cout << (int)test(45) << endl;
 	
 	std::vector<Argument> args;
 	f.compile_to_file("fib",args);
-
+	
+	save(test,"output.png");
 	return 0;
 }
