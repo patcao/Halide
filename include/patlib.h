a8bd145d7b55c73feed58b46fd2c492fec0d29c0
Patrick <patrick.cao8@gmail.com>
2014-07-26 17:51:33 -0400

    Updated timing functions, added test cases to cross compiled main

--- include/patlib.h
+++ include/patlib.h
@@ -3,7 +3,7 @@
 #include <sys/time.h>
 
 long start;
-int msec;
-#define timing(func,name) start = clock(); func; msec = (clock() - start) * 1000 / CLOCKS_PER_SEC; printf("%s Timing: %d ms \n",name, msec);
+long msec;
+#define timing(func,name) start = clock(); func; msec = (clock() - start) * 1000 / CLOCKS_PER_SEC; printf("%s Timing: %lu ms \n",name, msec);
 
 
